1baa9c35e67ae210a68c1d228f00f2cd09120748
newyoungbins <dudqlsjyb@naver.com>
2026-08-26 12:17:29 +0900

    이진탐색 알고리즘 진행

--- BinarySearch/Main.cpp
+++ BinarySearch/Main.cpp
@@ -1,8 +1,13 @@
 ﻿#include <iostream>
+#include <vector>
 
 // 재귀를 활용한 이진 탐색.
 int BinarySearchRecursive(const int* array, const int target, int start, int end)
 {
+	// 스택 실행 회수.
+	static int callCount = 0;
+	++callCount;
+	
 	// 종료 조건.
 	// 시작 인덱스가 종료 인덱스보다 커지면 검색 실패.
 	if (start > end)
@@ -29,6 +34,41 @@ int BinarySearchRecursive(const int* array, const int target, int start, int end
 	return BinarySearchRecursive(array, target, middle + 1, end);
 }
 
+// 반복적인(반복문) 방법으로 이진 탐색을 처리하는 함수.
+int BinarySearchIterative(const int* array, const int length, const int target)
+{
+	// 이진 탐색에 사용할 구간(범위).
+	int start = 0;
+	int end = length - 1;
+
+	while (start < end)
+	{
+		// 중간 인덱스.
+		int middle = (start + end) / 2;
+
+		// 찾았는지 검사.
+		if (array[middle] == target)
+		{
+			return middle;
+		}
+
+		// 중간값보다 작으면 왼쪽 검색.
+		if (array[middle] > target)
+		{
+			end = middle - 1;
+		}
+
+		// 중간값보다 크면 오른쪽 검색.
+		else
+		{
+			start = middle + 1;
+		}
+	}
+
+	// 탐색 실패.
+	return -1;
+}
+
 int main()
 {
 	// 배열.
@@ -37,7 +77,8 @@ int main()
 	const int target = 10;
 
 	// 인덱스 검색.
-	int result = BinarySearchRecursive(array, target, 0, length - 1);
+	//int result = BinarySearchRecursive(array, target, 0, length - 1);
+	int result = BinarySearchIterative(array, length, target);
 
 	// 검색 성공.
 	if (result != -1)
@@ -48,4 +89,6 @@ int main()
 	{
 		std::cout << "검색 실패.\n";
 	}
+
+	//std::bsearch()
 }
