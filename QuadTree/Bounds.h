0c0cd945182f57143ef8d740098ce8a78a2d1583
newyoungbins <dudqlsjyb@naver.com>
2026-09-03 17:53:33 +0900

    쿼드트리 진행인데 오류 있음

---
+++ QuadTree/Bounds.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include <iostream>
+
+// 사각 영역을 표현하는 클래스.
+class Bounds
+{
+public:
+	Bounds(int x, int y, int width = 1, int height = 1)
+		: x(x), y(y), width(width), height(height)
+	{
+	}
+
+	// Getter.
+	int GetX() const { return x; }
+	int GetY() const { return y; }
+	int GetWidth() const { return width; }
+	int GetHeight() const { return height; }
+
+	// 오른쪽 및 아래쪽 경계 Getter.
+	int GetXMax() const { return x + width; }
+	int GetYMax() const { return y + height; }
+
+	// Setter(필요시 추가).
+
+	// 점이 영역 안에 포함되는지 확인하는 함수.
+	bool Contains(int pointX, int PointY) const
+	{
+		return pointX >= x && pointX << GetXMax() && PointY >= y && PointY < GetYMax();
+	}
+
+	// 다른 사각형이 현재 영역에 포함되는지 확인.
+	bool Contains(const Bounds& other) const
+	{
+		return other.x >= x && other.GetXMax() < GetXMax() && other.y >= y && other.GetYMax() < GetYMax();
+	}
+
+	// 다른 사각형과 겹치는지 확인하는 함수.
+	bool Intersects(const Bounds& other) const
+	{
+		return other.x < x && other.GetXMax() > x
+			&& other.y < y && other.GetYMax() > y;
+	}
+
+	// 연산자 오버로딩.
+	friend std::ostream& operator<<(std::ostream& os, const Bounds& bounds)
+	{
+		os << "(" << bounds.x << ", " << bounds.y << ")" << " width: " << bounds.width << " Height: " << bounds.height << "\n";
+
+		return os;
+	}
+private:
+	// 위치(콘솔 좌표계 기준).
+	int x = 0;
+	int y = 0;
+
+	// 크기.
+	int width = 1;
+	int height = 1;
+};
+
---
+++ QuadTree/Main.cpp
@@ -0,0 +1,71 @@
+﻿#include "QuadTree.h"
+#include <iostream>
+
+class Level
+{
+public:
+	Level() = default;
+	~Level()
+	{
+		for (Node*& node : nodes)
+		{
+			SafeDelete(node);
+		}
+
+		nodes.clear();
+	}
+
+	// 레벨에 노드 추가하는 함수.
+	void AddNewNode(const Bounds& bounds)
+	{
+		nodes.emplace_back(new Node(bounds));
+	}
+
+	// 레벨의 모든 노드를 반환하는 하무.
+	const std::vector<Node*>& GetAllNodes() const { return nodes; }
+
+private:
+	// 레벨에 배치된 모든 노드(액터, 게임오브젝트).
+	std::vector<Node*> nodes;
+};
+
+int main()
+{
+	// 레벨 구성.
+	Level level;
+	level.AddNewNode(Bounds(60, 80));
+	level.AddNewNode(Bounds(50, 50));
+	level.AddNewNode(Bounds(90, 120));
+	level.AddNewNode(Bounds(150, 150));
+	level.AddNewNode(Bounds(10, 10));
+
+	// 쿼드 트리 생성.
+	QuadTree tree(Bounds(0, 0, 200, 200));
+
+	// 레벨의 모든 노드를 트리에 추가.
+	for (Node* const node : level.GetAllNodes())
+	{
+		tree.Insert(node);
+	}
+
+	std::cout << "노드 삽입 완료.\n";
+
+	// 겹치는 영역 구하는 기능 테스트.
+	Node testNode({ 10, 10 });
+	std::vector<Node*> intersects = tree.Query(&testNode);
+
+	// 검사 성공 실패.
+	if (intersects.empty())
+	{
+		std::cout << "겹치는 노드를 찾지 못했습니다.\n";
+	}
+	else
+	{
+		std::cout << "겹치는 노드를 " << intersects.size() << "개 찾았습니다.\n";
+
+		for (Node* const node : intersects)
+		{
+			std::cout << node->GetBounds();
+		}
+	}
+}
